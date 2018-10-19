e286c7f904415bc138465e4891fab3b1234fd51d
Anton Bannykh <Anton.Bannykh@jetbrains.com>
2018-10-19 14:54:07 +0300

    JS: test noInline version of inline suspend fun

---
+++ js/js.translator/testData/box/coroutines/onlyInlineSuspendFunction.kt
@@ -0,0 +1,42 @@
+// EXPECTED_REACHABLE_NODES: 1280
+
+// MODULE: lib
+// FILE: lib.kt
+import kotlin.coroutines.*
+
+var resume: () -> Unit = {}
+
+suspend fun dummy(msg: String): String {
+    return suspendCoroutine<String> { cont ->
+        resume = {
+            cont.resume(msg)
+        }
+    }
+}
+
+// MODULE: libInline(lib)
+// FILE: libInline.kt
+
+suspend inline fun foo(): String {
+    return dummy("O") + dummy("K")
+}
+
+// MODULE: main(libInline, lib)
+// FILE: main.kt
+
+import kotlin.coroutines.*
+
+fun box(): String {
+    var testResult: String = "fail"
+
+    val continuation = Continuation<String>(EmptyCoroutineContext) { result ->
+        testResult = result.getOrThrow()
+    }
+
+    // Test the noinline version of public inline suspend functions
+    js("libInline").foo(continuation)
+    resume()
+    resume()
+
+    return testResult
+}
